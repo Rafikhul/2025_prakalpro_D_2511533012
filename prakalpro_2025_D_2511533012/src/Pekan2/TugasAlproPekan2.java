51a9a021a061e82201741a5af337c5b55fd44d3f
Khul <159623218+Rafikhul@users.noreply.github.com>
2025-09-29 20:13:38 +0700

    Create TugasAlproPekan2.java

---
+++ prakalpro_2025_D_2511533012/src/Pekan2/TugasAlproPekan2.java
@@ -0,0 +1,27 @@
+package Pekan2;
+
+public class TuagsAlproPekan2 {
+
+	public static void main(String[] args) {
+		// cpu amd ryzen 5 9600X
+		
+		//validasi tipe data
+		int kelascpu = 5;
+		int gencpu = 9600 ;
+		float hargacpu = 4.4f;
+		char sericpu = 'x';
+		boolean gaming = true;
+		
+		
+		//output
+		System.out.println("=== Penjelasan AMD RYZEN 5 9600x "
+		+ "===");
+		System.out.println("Kelas pada CPU : \t" + kelascpu);
+		System.out.println("Generasi pada CPU : \t" + gencpu);
+		System.out.println("Harga jual CPU : \t" + hargacpu + " Juta");
+		System.out.println("Seri pada CPU : \t\t" + sericpu);
+		System.out.println("Perfoma gaming? : \t" + gaming);
+		
+	}
+
+}
