eb19bdb1683db2ea6d0dedc1f0819e6eea2fb624
Khul <159623218+Rafikhul@users.noreply.github.com>
2025-09-29 23:41:27 +0700

    Create TugasAlproPekan3.java

---
+++ prakalpro_2025_D_2511533012/src/Pekan3/TugasAlproPekan3.java
@@ -0,0 +1,30 @@
+package Pekan3;
+
+import java.util.Scanner;
+
+public class TugasAlproPekan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		double volume, r;
+
+        
+        do {
+        	// Input jari-jari
+        	System.out.print("Masukkan jari-jari bola: ");
+        	r = input.nextDouble();
+        
+        	if(r <= 0) {
+        		System.out.println("Jari-jari harus lebih besar dari 0!");
+        	}
+        } while (r <= 0);
+        	
+        // Hitung volume bola
+       	volume = (4.0 / 3.0) * 3.14 * r * r * r;
+       	System.out.println("Volume bola = " + volume);
+        
+	}
+
+}
