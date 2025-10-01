6c3285f180052dfe5cce1b935e84abf86ebbd19a
User <User@Khul-Property>
2025-10-01 13:18:48 +0700

    pekan3

--- prakalpro_2025_D_2511533012/src/Pekan3/TugasAlproPekan3.java
+++ prakalpro_2025_D_2511533012/src/Pekan3/TugasAlproPekan3.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Rafikhul/2025_prakalpro_D_2511533012
 package Pekan3;
 
 import java.util.Scanner;
@@ -28,3 +29,33 @@ public class TugasAlproPekan3 {
 	}
 
 }
+=======
+package Pekan3;
+
+import java.util.Scanner;
+
+public class TugasAlproPekan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		double volume;
+
+        // Input jari-jari
+        System.out.print("Masukkan jari-jari bola: ");
+        double r = input.nextDouble();
+        
+        if(r > 0) {
+        	// Hitung volume bola
+        	volume = (4.0 / 3.0) * 3.14 * r * r * r;
+        	System.out.println("Volume bola = " + volume);
+        }else {
+            System.out.println("Jari-jari harus lebih besar dari 0!");
+        }
+        
+        input.close();
+	}
+
+}
+>>>>>>> ac8a8a0 Tugas Alpro Pekan3
