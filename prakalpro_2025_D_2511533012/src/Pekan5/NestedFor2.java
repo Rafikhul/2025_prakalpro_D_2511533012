06e9212ae2f3636e19588d7d9fa8b0789dcdf1d2
Khul <159623218+Rafikhul@users.noreply.github.com>
2025-10-29 14:26:23 +0700

    prakalpro pekan 5

---
+++ prakalpro_2025_D_2511533012/src/Pekan5/NestedFor2.java
@@ -0,0 +1,33 @@
+package Pekan5;
+
+public class NestedFor2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i = 0; i < 15; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+		
+        for(int i = 0; i < 6; i++) {
+            System.out.print("* "); // border kiri
+            for(int j = 0; j < 6; j++) {
+                System.out.print(i + j + " ");
+            }
+            System.out.println("*"); // border kanan
+        }
+		
+        for(int i = 0; i < 15; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+		for (int i = 0; i <=5; i++) {
+			for (int j = 0; j <=5; j++) {
+				System.out.print(i+j+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
