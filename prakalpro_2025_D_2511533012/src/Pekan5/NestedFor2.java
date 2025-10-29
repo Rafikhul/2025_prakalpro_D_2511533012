96c2f2da1a1b0cc721aab022b12c673337e7b1a2
Khul <159623218+Rafikhul@users.noreply.github.com>
2025-10-29 14:48:35 +0700

    alpro

--- prakalpro_2025_D_2511533012/src/Pekan5/NestedFor2.java
+++ prakalpro_2025_D_2511533012/src/Pekan5/NestedFor2.java
@@ -24,7 +24,7 @@ public class NestedFor2 {
         
 		for (int i = 0; i <=5; i++) {
 			for (int j = 0; j <=5; j++) {
-				System.out.print(i+j+ " ");
+				System.out.print(i + j + " ");
 			}
 			System.out.println();
 		}
